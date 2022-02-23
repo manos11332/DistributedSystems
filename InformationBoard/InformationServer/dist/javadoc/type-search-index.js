21752310141d4a6a6e9f4b39febbfd63192f0f99
manos-galanis <63466547+manos11332@users.noreply.github.com>
2022-02-23 03:41:17 +0200

    Add files via upload

---
+++ InformationBoard/InformationServer/dist/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Announcement"},{"p":"<Unnamed>","l":"ChangeIndex"},{"p":"<Unnamed>","l":"Login"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Person"},{"p":"<Unnamed>","l":"Register"},{"p":"<Unnamed>","l":"SerializationUtil"}]
